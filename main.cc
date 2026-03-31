0f18054126b3ff7e22029dfb15295d1e00b0687d
debugee <519306795@qq.com>
2026-03-31 15:54:09 +0800

    REL_3_0_FINAL original files

--- main.cc
+++ main.cc
@@ -1,5 +1,8 @@
-#include "bochs.h"
-#include "instr.h"
+#include <bochs.h>
+#include <instr.h>
+#include <decoder.h>
+#include <fetchdecode.h>
+#include <iostream>
 
 extern int fetchDecode32(const Bit8u *fetchPtr, bool is_32, bxInstruction_c *i, unsigned remainingInPage);
 #if BX_SUPPORT_X86_64
@@ -9,16 +12,41 @@ extern int fetchDecode64(const Bit8u *fetchPtr, bxInstruction_c *i, unsigned rem
 int main()
 {
     char disbuf[100] = {0};
-    Bit8u instr[] = {0x29,0xc0};
+    Bit8u instr[] = {0x06};
     bxInstruction_c i = {0};
     bx_address cs_base = 0;
     bx_address ip = 0;
-    //int ret = fetchDecode64(instr, &i, 3);
-
-    if (disbuf == disasm(instr, 0, 1, disbuf, &i, cs_base, ip, BX_DISASM_INTEL))
+    if (disbuf == disasm(instr, 0, 0, disbuf, &i, cs_base, ip, BX_DISASM_INTEL))
     {
         i.set_opcode_bytes(instr);
+        std::cout << i.ilen() << std::endl;
+        std::cout << disbuf << std::endl;
     }
-
+    //    extern bxIAOpcodeTable BxOpcodesTable[];
+    //    Bit16u ia_opcode = i.getIaOpcode();
+    //    int lenImm = 0;
+    // 			for (unsigned n = 0; n <= 3; n++) {
+    //     			unsigned type_src = (unsigned)BxOpcodesTable[ia_opcode].src[n];
+    // 				unsigned type = BX_DISASM_SRC_TYPE(type_src);
+    // 				unsigned src = BX_DISASM_SRC_ORIGIN(type_src);
+    // 				if (src == BX_SRC_IMM)
+    // 				{
+    // 					switch(type) {
+    // 							case BX_IMM1:
+    // 							case BX_IMMB:
+    // 							case BX_IMMBW_SE:
+    // 							case BX_IMMBD_SE:
+    // 							case BX_IMMB2:
+    // 								lenImm++;
+    // 								break;
+    // 							case BX_IMMW:
+    // 								lenImm += 2;
+    // 								break;
+    // 							case BX_IMMD:
+    // 								lenImm += 4;
+    // 					}
+    // 				}
+    // 			}
+    //             std::cout << lenImm << std::endl;
     return 0;
 }
--- makefile
+++
@@ -1,8 +0,0 @@
-.PHONY : clean
-
-CXXFLAGS := $(CXXFLAGS) -g -DBX_STANDALONE_DECODER -DBX_INSTR_STORE_OPCODE_BYTES
-objects = disasm.o fetchdecode32.o fetchdecode64.o main.o
-decoder: $(objects)
-	cc -o decoder $(objects)
-clean :
-	rm decoder $(objects)
